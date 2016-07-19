4d5d017e99d8a7527481adef50f695ca268fe0ef
DrabWeb <sethunity@gmail.com>
2016-07-19 20:23:05 -0300

    Added empty project

---
+++ MoeMAL/MoeMAL/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MoeMAL
+//
+//  Created by Seth on 2016-07-19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
